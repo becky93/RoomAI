04d0f2a47dbb1a6725f825161677965ee3730d97
sifaxie <sifaxie@tencent.com>
2017-11-06 17:22:11 +0800

    add Sevenking Player Class

--- testSevenKing.py
+++ testSevenKing.py
@@ -122,11 +122,11 @@ class AlwaysMaxPlayer(roomai.common.AbstractPlayer):
                 max_action = SevenKingAction.lookup("")
                 max_pattern = 0
                 for a in self.available_actions.values():
-                    if(a.pattern[1]>max_pattern):
+                    if (a.pattern[1] > max_pattern):
                         max_pattern = a.pattern[1]
                         max_action = a
-                    elif(a.pattern == max_pattern):
-                        if(a.pattern[0] != 'p_0' and a.cards[-1]>max_action.cards[-1]):
+                    elif (a.pattern[1] == max_pattern):
+                        if (a.pattern[0] != 'p_0' and (SevenKingPokerCard.compare(a.cards[-1], max_action.cards[-1]) > 0)):
                             max_action = a
 
                 return max_action
@@ -146,43 +146,6 @@ class AlwaysMaxPlayer(roomai.common.AbstractPlayer):
                 """
                 pass
 
-            class AlwaysMinPlayer(roomai.common.AbstractPlayer):
-                """
-                """
-
-                def take_action(self):
-                    """
-
-                    Returns:
-
-                    """
-                    min_card = None
-                    for a in self.available_actions.values():
-                        if a.pattern[0] == "p_1":
-                            if min_card is None:
-                                min_card = a.cards[0]
-                            else:
-                                card = a.cards[0]
-                                if SevenKingPokerCard.compare(card, min_card) < 0: min_card = card
-                    if min_card is None:
-                        return list(self.available_actions.values())[0]
-                    else:
-                        return SevenKingAction.lookup(min_card.key)
-
-                def receive_info(self, info):
-                    """
-
-                    Args:
-                        info:
-                    """
-                    self.public_state = info.public_state
-                    self.available_actions = info.person_state.available_actions
-
-                def reset(self):
-                    """
-
-                    """
-                    pass
 
 class testSevenKing(unittest.TestCase):
     """
@@ -209,9 +172,9 @@ class testSevenKing(unittest.TestCase):
         print ("available_actions=",person_states[turn].available_actions.keys())
         print ("available_actions_v=",person_states[turn].available_actions.values())
 
-
+#      it may go wrong,
         action = SevenKingAction("%s,%s" % (person_states[turn].hand_cards[0].key, person_states[turn].hand_cards[1].key))
-        # infos, public_state, person_states, private_state = env.forward(action)
+        infos, public_state, person_states, private_state = env.forward(action)
 
 
     def testRandom(self):
