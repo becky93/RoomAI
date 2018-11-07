d20f881af4a8f536a6083d23012695d2eb09f337
algorithmdog <lili1987mail@gmail.com>
2018-11-07 11:44:34 +0800

    complete cards

--- roomai/games/bang/BangStateUtil.py
+++ roomai/games/bang/BangStateUtil.py
@@ -57,12 +57,17 @@ class BangCardNames:
     Prigione    = "Prigione"
     Saloon      = "Saloon"
     Beer        = "Beer"
+    Catling     = "Catling"
+    CatBalou    = "CatBalou"
+    Miss        = "Miss"
+    StageCoach  = "StageCoach"
+    Barrel      = "Barrel"
 
 class BangCardSuits:
-    Spade   = "Spade"
+    Club    = "Club"
     Heart   = "Heart"
     Diamond = "Diamond"
-    Club    = "Club"
+    Spade   = "Spade"
 
 class BangCardColors:
     Blue = "Blue"
@@ -145,15 +150,16 @@ class BangCard(object):
 
 
 AllPokerCards = dict()
-AllPokerCards["%s-A-%s"%(BangCardNames.Carabine, BangCardSuits.Spade)] = BangCard(BangCardNames.Carabine,"A",BangCardSuits.Spade, BangCardColors.Blue)
+
+AllPokerCards["%s-A-%s" % (BangCardNames.Carabine, BangCardSuits.Club)] = BangCard(BangCardNames.Carabine, "A", BangCardSuits.Club, BangCardColors.Blue)
 for i in range(2,8):
-    AllPokerCards["%s-%d-%s"%(BangCardNames.Duello,i, BangCardSuits.Spade)] = BangCard(BangCardNames.Bang,"%d" % (i), BangCardSuits.Spade, BangCardColors.Brown)
-AllPokerCards["%s-8-%s"%(BangCardNames.Duello,BangCardSuits.Spade)] = BangCard(BangCardNames.Duello,"8", BangCardSuits.Spade, BangCardColors.Brown)
-AllPokerCards["%s-9-%s"%(BangCardNames.Emporia,BangCardSuits.Spade)] = BangCard(BangCardNames.Emporia,"9", BangCardSuits.Spade, BangCardColors.Brown)
-AllPokerCards["%s-10-%s"%(BangCardNames.Volcanic,BangCardSuits.Spade)] = BangCard(BangCardNames.Volcanic,"10",BangCardSuits.Spade,BangCardColors.Blue)
-AllPokerCards["%s-J-%s"%(BangCardNames.Schofield,BangCardSuits.Spade)] = BangCard(BangCardNames.Schofield,"J",BangCardSuits.Spade,BangCardColors.Blue)
-AllPokerCards["%s-Q-%s"%(BangCardNames.Schofield,BangCardSuits.Spade)] = BangCard(BangCardNames.Schofield,"Q",BangCardSuits.Spade,BangCardColors.Blue)
-AllPokerCards["%s-K-%s"%(BangCardNames.Remington,BangCardSuits.Spade)] = BangCard(BangCardNames.Remington,"K",BangCardSuits.Spade,BangCardColors.Blue)
+    AllPokerCards["%s-%d-%s" % (BangCardNames.Duello,i, BangCardSuits.Club)] = BangCard(BangCardNames.Bang, "%d" % (i), BangCardSuits.Club, BangCardColors.Brown)
+AllPokerCards["%s-8-%s" % (BangCardNames.Duello,BangCardSuits.Club)] = BangCard(BangCardNames.Duello, "8", BangCardSuits.Club, BangCardColors.Brown)
+AllPokerCards["%s-9-%s" % (BangCardNames.Emporia,BangCardSuits.Club)] = BangCard(BangCardNames.Emporia, "9", BangCardSuits.Club, BangCardColors.Brown)
+AllPokerCards["%s-10-%s" % (BangCardNames.Volcanic,BangCardSuits.Club)] = BangCard(BangCardNames.Volcanic, "10", BangCardSuits.Club, BangCardColors.Blue)
+AllPokerCards["%s-J-%s" % (BangCardNames.Schofield,BangCardSuits.Club)] = BangCard(BangCardNames.Schofield, "J", BangCardSuits.Club, BangCardColors.Blue)
+AllPokerCards["%s-Q-%s" % (BangCardNames.Schofield,BangCardSuits.Club)] = BangCard(BangCardNames.Schofield, "Q", BangCardSuits.Club, BangCardColors.Blue)
+AllPokerCards["%s-K-%s" % (BangCardNames.Remington,BangCardSuits.Club)] = BangCard(BangCardNames.Remington, "K", BangCardSuits.Club, BangCardColors.Blue)
 
 AllPokerCards["%s-A-%s"%(BangCardNames.Panic,BangCardSuits.Heart)]          = BangCard(BangCardNames.Panic,         "A",    BangCardSuits.Heart,    BangCardColors.Brown)
 AllPokerCards["%s-2-%s"%(BangCardNames.Dynamite,BangCardSuits.Heart)]       = BangCard(BangCardNames.Dynamite,      "2",    BangCardSuits.Heart,    BangCardColors.Blue)
@@ -161,4 +167,24 @@ AllPokerCards["%s-3-%s"%(BangCardNames.WellsFargo,BangCardSuits.Heart)]     = Ba
 AllPokerCards["%s-4-%s"%(BangCardNames.Prigione,BangCardSuits.Heart)]       = BangCard(BangCardNames.Prigione,      "4",    BangCardSuits.Heart,    BangCardColors.Blue)
 AllPokerCards["%s-4-%s"%(BangCardNames.Saloon,BangCardSuits.Heart)]         = BangCard(BangCardNames.Saloon,        "5",    BangCardSuits.Heart,    BangCardColors.Brown)
 for i in range(4,10):
-    AllPokerCards["%s-i-%s" % (BangCardNames.Beer,i, BangCardSuits.Heart)] = BangCard(BangCardNames.Beer, "%d"%(i), BangCardSuits.Heart,   BangCardColors.Brown)
+    AllPokerCards["%s-i-%s" % (BangCardNames.Beer,i, BangCardSuits.Heart)]  = BangCard(BangCardNames.Beer,          "%d"%(i), BangCardSuits.Heart,   BangCardColors.Brown)
+AllPokerCards["%s-10-%s"%(BangCardNames.Catling,BangCardSuits.Heart)]       = BangCard(BangCardNames.Catling,       "10",     BangCardSuits.Heart,  BangCardColors.Brown)
+AllPokerCards["%s-J-%s"%(BangCardNames.Beer,BangCardSuits.Heart)]           = BangCard(BangCardNames.Beer,          "J",      BangCardSuits.Heart,  BangCardColors.Brown)
+AllPokerCards["%s-Q-%s"%(BangCardNames.Bang,BangCardSuits.Heart)]           = BangCard(BangCardNames.Bang,          "Q",      BangCardSuits.Heart,  BangCardColors.Brown)
+AllPokerCards["%s-Q-%s"%(BangCardNames.CatBalou,BangCardSuits.Heart)]       = BangCard(BangCardNames.CatBalou,      "K",      BangCardSuits.Heart,  BangCardColors.Brown)
+
+AllPokerCards["%s-A-%s"%(BangCardNames.Bang,BangCardSuits.Spade)]          = BangCard(BangCardNames.Bang,         "A",    BangCardSuits.Spade,    BangCardColors.Brown)
+for i in range(2,9):
+    AllPokerCards["%s-i-%s" % (BangCardNames.Miss,i, BangCardSuits.Spade)]  = BangCard(BangCardNames.Miss,           "%d"%(i),  BangCardSuits.Spade,    BangCardColors.Brown)
+AllPokerCards["%s-9-%s"%(BangCardNames.StageCoach,BangCardSuits.Spade)]     = BangCard(BangCardNames.StageCoach,      "9",      BangCardSuits.Spade,    BangCardColors.Brown)
+AllPokerCards["%s-10-%s"%(BangCardNames.Prigione,BangCardSuits.Spade)]      = BangCard(BangCardNames.Prigione,        "10",     BangCardSuits.Spade,    BangCardColors.Blue)
+AllPokerCards["%s-J-%s"%(BangCardNames.Prigione,BangCardSuits.Spade)]       = BangCard(BangCardNames.Prigione,        "J",       BangCardSuits.Spade,    BangCardColors.Blue)
+AllPokerCards["%s-Q-%s"%(BangCardNames.Barrel,BangCardSuits.Spade)]       = BangCard(BangCardNames.Barrel,        "Q",           BangCardSuits.Spade,    BangCardColors.Brown)
+AllPokerCards["%s-K-%s"%(BangCardNames.Schofield,BangCardSuits.Spade)]       = BangCard(BangCardNames.Schofield,        "K",       BangCardSuits.Spade,    BangCardColors.Blue)
+
+AllPokerCards["%s-A-%s"%(BangCardNames.Bang,BangCardSuits.Diamond)]          = BangCard(BangCardNames.Bang,             "A",    BangCardSuits.Diamond,    BangCardColors.Brown)
+for i in range(2,11):
+    AllPokerCards["%s-i-%s" % (BangCardNames.Bang,i, BangCardSuits.Diamond)]  = BangCard(BangCardNames.Bang,           "%d"%(i),  BangCardSuits.Diamond,    BangCardColors.Brown)
+AllPokerCards["%s-J-%s"%(BangCardNames.Bang,BangCardSuits.Spade)]       = BangCard(BangCardNames.Bang,        "J",       BangCardSuits.Diamond,    BangCardColors.Brown)
+AllPokerCards["%s-Q-%s"%(BangCardNames.Bang,BangCardSuits.Spade)]       = BangCard(BangCardNames.Bang,        "Q",       BangCardSuits.Diamond,    BangCardColors.Brown)
+AllPokerCards["%s-K-%s"%(BangCardNames.Bang,BangCardSuits.Spade)]       = BangCard(BangCardNames.Bang,        "K",       BangCardSuits.Diamond,    BangCardColors.Brown)
